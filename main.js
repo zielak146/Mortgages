be1ca0eb71316676b5e9e7463e6abd69100ffe5e
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-10-07 18:05:16 +0100

    Upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const navItems = document.querySelectorAll('.nav_items');
+const navBtn = document.querySelector('.burger-btn');
+const navBar = document.querySelector('.nav');
+
+const handleNav = () => {
+	navBtn.addEventListener('click', () => {
+		navBar.classList.toggle('active');
+	});
+};
+
+handleNav();
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navBar.classList.remove('active')
+    })
+})
