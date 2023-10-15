4bed1abb59198680ae3c757b76f03be7d6b01e78
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-10-15 13:40:53 +0100

    Arrow up added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const navItems = document.querySelectorAll('.nav_items');
 const navBtn = document.querySelector('.burger-btn');
 const navBar = document.querySelector('.nav');
+const arrowUp = document.querySelector('.arrow-up');
 
 const handleNav = () => {
 	navBtn.addEventListener('click', () => {
@@ -15,3 +16,18 @@ navItems.forEach((item) => {
 		navBar.classList.remove('active');
 	});
 });
+
+let scrollPos = 600;
+
+function checkPosition() {
+	if (scrollY >= scrollPos) {
+		// Scrolling UP
+		arrowUp.classList.add('visible');
+	} else {
+		arrowUp.classList.remove('visible');
+	}
+
+	scrollPos = scrollY;
+}
+
+window.addEventListener('scroll', checkPosition);
