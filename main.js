c255d4e3be6eea76a5de6dc27ba3b49cca77c37b
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-10-15 16:36:03 +0100

    Transition of sections

--- main.js
+++ main.js
@@ -31,3 +31,19 @@ function checkPosition() {
 }
 
 window.addEventListener('scroll', checkPosition);
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} 
+		// else {
+		// 	entry.target.classList.remove('show');
+		// }
+	});
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
