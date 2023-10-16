a9bde4dacf45602dbfdf210ea3583d15c42022db
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-10-16 09:30:41 +0100

    Slider Added

--- main.js
+++ main.js
@@ -47,3 +47,56 @@ const observer = new IntersectionObserver((entries) => {
 
 hiddenElements.forEach((el) => observer.observe(el));
 
+
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+enabled:'true',
+minimumVelocity: 3,
+momentum:'true',
+momentumBounce: 'true',
+momentumRatio: 1,
+momentumBounceRatio:1,
+momentumVelocityRatio:1,
+  
+	// If we need pagination
+	// pagination: {
+	//   el: '.swiper-pagination',
+	// },
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+
+  
+	// And if we need scrollbar
+	// scrollbar: {
+	//   el: '.swiper-scrollbar',
+	// },
+	slidesPerView: 1,
+	spaceBetween: 1,
+	// Responsive breakpoints
+	breakpoints: {
+	  // when window width is >= 320px
+	  767: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  // when window width is >= 480px
+	  992: {
+		slidesPerView: 2,
+		spaceBetween:0,
+	  },
+	  1300: {
+		slidesPerView: 3,
+		spaceBetween:0,
+	  },
+	  // when window width is >= 640px
+	 
+	}
+  });
+  
+ 
